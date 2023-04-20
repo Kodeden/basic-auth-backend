1b7410ed177de80ddf740026cd3c26e58cb151ff
Manav Misra <manavm@visionwebsoft.com>
2023-04-20 11:05:05 -0500

    Remove 🔥 unused test file

--- app/index.test.js
+++
@@ -1,7 +0,0 @@
-import assert from "node:assert";
-import { test } from "node:test";
-import { add2Nums } from "./index.js";
-
-test("1 + 1 = 2", () => {
-  assert.strictEqual(add2Nums(1, 1), 2);
-});
