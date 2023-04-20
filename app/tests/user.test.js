3f1024d3f6375bb0a860236cc23f8167b45f1e1b
Manav Misra <manavm@visionwebsoft.com>
2023-04-20 11:35:50 -0500

    test: returns a 400 with an appropriate error message if the password is too short

---
+++ app/tests/user.test.js
@@ -0,0 +1,31 @@
+import assert from "node:assert";
+import { before, describe, it } from "node:test";
+import request from "supertest";
+import { setupServer } from "../server.js";
+import userSchema from "../user/model.js";
+
+const sadPath = {
+  username: "mark",
+  password: "west",
+};
+
+describe("User routes", () => {
+  let app;
+
+  before(() => {
+    app = setupServer();
+  });
+
+  describe("POST /users/register", () => {
+    it("should return a '400' with an appropriate error message if the password is too short", async () => {
+      const response = await request(app).post("/users/register").send(sadPath);
+
+      assert.equal(response.status, 400);
+      // Does the error message contains the expected keywords from the schema?
+      assert.match(
+        response.body.error,
+        new RegExp(userSchema.password.isLength.errorMessage)
+      );
+    });
+  });
+});
