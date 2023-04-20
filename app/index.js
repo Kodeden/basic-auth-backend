67c6cb1570ba1a37b3e5217bf48f9bd0893b0e4c
Manav Misra <manavm@visionwebsoft.com>
2023-04-20 08:17:00 -0500

    feat: start express server
    Uses port from config.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import config from "./config.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(config.port, () => {
+  console.info(`Listening on port ${config.port}`);
+});
