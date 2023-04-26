1ccad35498e0a78dea8d38aedd422a760039e01c
Manav Misra <manavm@visionwebsoft.com>
2023-04-26 17:05:33 -0500

    Enable CORS

--- app/server.js
+++ app/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import config from "./config.js";
 import handleError from "./middleware/handle-error.js";
 import usersRoutes from "./user/routes.js";
+import cors from "cors";
 
 export const setupServer = () => {
   const app = express();
 
+  // Enable CORS for all routes ⚠️
+  app.use(cors());
   app.use(express.json());
 
   app.use("/users", usersRoutes);
